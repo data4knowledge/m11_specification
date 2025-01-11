858e06ed85f3b329c99f897bc653dbb9d5b441e3
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-01-11 16:56:54 +0100

    Adding in instructional text

--- m11_template/m11_template.py
+++ m11_template/m11_template.py
@@ -2,8 +2,8 @@ import re
 from pathlib import Path
 from raw_docx import RawDocx, RawDocument, RawParagraph, RawTable, RawSection
 
-class M11Template:
 
+class M11Template:
     def __init__(self, filepath: str):
         """
         Initialize the M11 processor with paths to both specification documents.
@@ -12,7 +12,7 @@ class M11Template:
             filepath (str): Path to the Template Specification PDF
         """
         self.document = None
-        self.elements = []
+        self.elements = {}
         self.filepath = Path(filepath)
         if not self.filepath.exists():
             raise FileNotFoundError(
@@ -27,18 +27,51 @@ class M11Template:
         self.document = raw_doc.to_dict()
         for section in raw_doc.sections:
             for item in section.items:
+                last_document_elements = []
                 if isinstance(item, RawParagraph):
                     confirmed_elements = self._extract_elements(section, item)
-                    self.elements += confirmed_elements
+                    if confirmed_elements:
+                        self._add_elements(confirmed_elements)
+                        last_document_elements = confirmed_elements
+                    else:
+                        instructions = self._extract_instructions(section, item)
+                        self._add_instructions(last_document_elements, instructions)
+                        last_document_elements = []
                 elif isinstance(item, RawTable):
                     for row in item.rows:
                         for cell in row.cells:
+                            last_row_elements = []
                             for item in cell.items:
                                 if isinstance(item, RawParagraph):
-                                    confirmed_elements = self._extract_elements(section, item)
-                                    self.elements += confirmed_elements
-    
-    def _extract_elements(self, section: RawSection, paragraph: RawParagraph) -> list[str]:
+                                    confirmed_elements = self._extract_elements(
+                                        section, item
+                                    )
+                                    self._add_elements(confirmed_elements)
+                                    last_row_elements = confirmed_elements
+                                    instructions = self._extract_instructions(
+                                        section, item
+                                    )
+                                    self._add_instructions(
+                                        last_row_elements, instructions
+                                    )
+
+    def _add_elements(self, elements: list[str]) -> None:
+        """
+        Add elements to the elements dictionary.
+        """
+        for element in elements:
+            self.elements[element["short_name"]] = element
+
+    def _add_instructions(self, elements: list[str], instructions: list[str]) -> None:
+        """
+        Add instructions to the elements dictionary.
+        """
+        for element in elements:
+            element["instructions"] = instructions
+
+    def _extract_elements(
+        self, section: RawSection, paragraph: RawParagraph
+    ) -> list[str]:
         """
         Extract elements from a paragraph.
 
@@ -52,20 +85,55 @@ class M11Template:
         confirmed_elements = []
         potential_elements = self._find_elements(paragraph.text)
         for element in potential_elements:
-            print(f"TEXT: {[x.text for x in paragraph.runs]}")
-            print(f"COLOR: {[x.color for x in paragraph.runs]}")
-            print(f"HIGHLIGHT: {[x.highlight for x in paragraph.runs]}")
-            print(f"STYLE: {[x.style for x in paragraph.runs]}")
-            match = next((x for x in paragraph.runs if element in x.text and x.highlight == 'GRAY_25 (16)'), None)
+            # print(f"TEXT: {[x.text for x in paragraph.runs]}")
+            # print(f"COLOR: {[x.color for x in paragraph.runs]}")
+            # print(f"HIGHLIGHT: {[x.highlight for x in paragraph.runs]}")
+            # print(f"STYLE: {[x.style for x in paragraph.runs]}")
+            match = next(
+                (
+                    x
+                    for x in paragraph.runs
+                    if element in x.text and x.highlight == "GRAY_25 (16)"
+                ),
+                None,
+            )
             if match:
                 short_name = element.replace("Enter ", "")
-                confirmed_elements.append({'long_name': element, 'short_name': short_name, 'section_number': section.number, 'section_title': section.title})
-        print(f"CONFIRMED ELEMENTS: {confirmed_elements}")  
+                optional = True if match.color == "3333FF" else False
+                confirmed_elements.append(
+                    {
+                        "long_name": element,
+                        "short_name": short_name,
+                        "optional": optional,
+                        "section_number": section.number,
+                        "section_title": section.title,
+                        "optional": optional,
+                        "instructions": []
+                    }
+                )
+        print(f"CONFIRMED ELEMENTS: {confirmed_elements}")
         return confirmed_elements
-    
+
+    def _extract_instructions(
+        self, section: RawSection, paragraph: RawParagraph
+    ) -> list[str]:
+        """
+        Extract instructions from a paragraph.
+
+        Args:
+            section (RawSection): The section the paragraph belongs to
+            paragraph (RawParagraph): The paragraph to extract instructions from
+
+        Returns:
+            list[str]: The instructions
+        """
+        instructions = [x.text for x in paragraph.runs if x.color == "C00000"]
+        print(f"INSTRUCTIONS: {instructions}")
+        return instructions
+
     def _find_elements(self, text: str) -> list[str]:
         """
-        Find elements in a paragraph. 
+        Find elements in a paragraph.
         Elements are found by looking for text between < and >.
         Args:
             text (str): The text to find elements in
@@ -73,7 +141,7 @@ class M11Template:
         Returns:
             list[str]: A list of elements
         """
-        pattern = r'<([^>]+)>'
+        pattern = r"<([^>]+)>"
         matches = re.findall(pattern, text)
-        print(f"MATCHES: {matches}")
+        # print(f"MATCHES: {matches}")
         return matches
--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from m11_template.m11_template import M11Template
 
 template = M11Template(filepath="data/input_data/m11-template-spec.docx")
 template.process()
-with open("data/output_data/m11.json", 'w') as f:
+with open("data/output_data/m11.json", "w") as f:
     json.dump(template.document, f, indent=4)
-with open("data/output_data/elements.json", 'w') as f:
+with open("data/output_data/elements.json", "w") as f:
     json.dump(template.elements, f, indent=4)
-
