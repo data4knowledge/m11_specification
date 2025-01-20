d445adcad220b970cfd81241864cd51ed58c32d8
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-01-20 11:16:57 +0100

    First cut USDM mapping. Moved some files around to keep separate

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import json
+import yaml
 from m11_template.m11_template import M11Template
 from m11_template.m11_technical import M11Technical
 from tabulate import tabulate
 from itertools import zip_longest
 
 
-template = M11Template(filepath="data/input_data/m11-template-spec.docx")
+template = M11Template(filepath="data/input_data/m11/m11-template-spec.docx")
 template.process()
-template.rename_elements(filepath="data/input_data/template_renames.yaml")
-technical = M11Technical(filepath="data/input_data/m11-technical-spec.docx")
+template.rename_elements(filepath="data/input_data/m11/template_renames.yaml")
+technical = M11Technical(filepath="data/input_data/m11/m11-technical-spec.docx")
 technical.process()
-technical.rename_elements(filepath="data/input_data/technical_renames.yaml")
+technical.rename_elements(filepath="data/input_data/m11/technical_renames.yaml")
 
 with open("data/output_data/template_document.json", "w") as f:
     json.dump(template.document, f, indent=4)
@@ -36,6 +37,13 @@ for key, value in technical.elements.items():
     if key not in template.elements:
         missing['technical'][key] = value
         #print(f"MISSING TECHNICAL: {key}")
+
+with open("data/input_data/usdm/m11_mapping.yaml", "r") as f:
+    usdm = yaml.load(f, Loader=yaml.FullLoader)
+
+for key, value in merged.items():
+    if key in usdm:
+        merged[key]['usdm'] = usdm[key]
 
 with open("data/output_data/merged_elements.json", "w") as f:
     json.dump(merged, f, indent=4)
